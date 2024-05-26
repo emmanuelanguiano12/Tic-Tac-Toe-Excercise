1f9daff5d72d4f8cf575e6e2974f97c8d0e99269
emmanuelanguiano12 <emmanuelanguiano1223@gmail.com>
2024-05-26 12:53:51 -0600

    LocalStorage Update

---
+++ src/logic/board.js
@@ -0,0 +1,18 @@
+import { WINNER_COMBOS } from "../constants"
+
+export const checkWinnerFrom = (boardCheck) => {
+    for(const combo of WINNER_COMBOS){
+      const [a, b, c] = combo
+      if(
+        boardCheck[a] && 
+        boardCheck[a] === boardCheck[b] 
+        && boardCheck[a] === boardCheck[c]){
+        return boardCheck[a]
+      }
+    }
+    return null
+  }
+
+  export const checkEndGame = (newBoard) => {
+    return newBoard.every(square => square !== null)
+  }
